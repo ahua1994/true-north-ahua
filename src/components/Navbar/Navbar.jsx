daf6f0c8ae408337bd6447b368a9af49006cbbf4
Andy Hua <user.andy.hua@gmail.com>
2022-10-27 14:00:52 -0700

    finished navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,27 +4,29 @@ import "./Navbar.scss";
 const Navbar = () => {
     return (
         <div className="Navbar">
-            <Link to="/">
-                <div className="logo">
-                    <h1>
-                        <span>True North</span> Martial Arts
-                    </h1>
-                </div>
-            </Link>
+            <div className="logo">
+                <h1>
+                    <Link to="/">
+                        <span>True North</span>
+                    </Link>{" "}
+                    Martial Arts
+                </h1>
+            </div>
+
             <div className="right">
                 <Link to="/">
                     <div className="home">
                         <h1>Home</h1>
                     </div>
                 </Link>
-                <Link to="/about">
-                    <div className="about">
-                        <h1>About</h1>
-                    </div>
-                </Link>
                 <Link to="/programs">
                     <div className="programs">
                         <h1>Programs</h1>
+                    </div>
+                </Link>
+                <Link to="/about">
+                    <div className="about">
+                        <h1>About</h1>
                     </div>
                 </Link>
             </div>
